6ccb6cd9b7095d3ffd5f1f63db0164cea87a3d11
Du <1034331038@qq.com>
2023-01-14 17:07:12 -0500

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # WasItRufus
+
+### Get started
+### How to run?
+### 0. Make sure you have python installed
+### 1. There are two ways to run
+### Method1:
+Copy and paste your .git file path to main.py line 24. For example
+```python
+git_dir = '/Users/dukaixuan/PycharmProjects/WasItRufus/.git'
+```
+Then use the command 
+```shell
+python main.py
+```
+ to get the result.
+
+### Method2:
+Pass the path when start the program. For example
+```shell
+python main.py /Users/dukaixuan/PycharmProjects/WasItRufus/.git
+```
+ to get the result.
+
  
--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import time
 import git
+import pathlib
+import sys
+
 
 def wasItRufus(git_dir):
-    pass
+    if not pathlib.Path(git_dir).exists():
+        print('.git file not found, please check path')
+        return
+    repo = git.Repo(git_dir)
+    repo.config_reader()
+    headcommit = repo.head.commit
+    date = headcommit.authored_date
+    cur = time.time()
+    inOneWeek = (cur - date) <= 604800
+    print('active branch:', repo.active_branch)
+    print('local changes:', repo.is_dirty())
+    print('recent commit:', inOneWeek)
+    print('blame Rufus:', headcommit.author.name == 'Rufus')
 
+
+if __name__ == '__main__':
+    # change with your .git file path
+    git_dir = '/Users/dukaixuan/PycharmProjects/WasItRufus/.git'
+    if len(sys.argv) >= 2 and sys.argv[1]:
+        git_dir = sys.argv[1]
+    print(git_dir)
+    wasItRufus(git_dir)
